da18e4415f9dc04c2a87b534dbc9bb36ec3644c0
feiyuerenhai <454570466@qq.com>
2018-12-11 17:37:50 +0800

    feature:build

--- src/Person.js
+++ src/Person.js
@@ -1,5 +1,5 @@
 function country(c) {
-	return function(target, name, descriptor){
+	return function (target, name, descriptor) {
 		target.country = c;
 	}
 }
@@ -8,22 +8,22 @@ const s1 = Symbol.for('name');
 const s2 = Symbol.for('name');
 
 const o = {
-	[s1]: 'hello',
+	[s1]: 'testValue',
 };
 
 debugger;
 
-console.log( s1 == s2, o[s1] == o[s2] );
+console.log(s1 == s2, o[s1] == o[s2]);
 
 const m = new Map();
 
 @country('U.S.')
-export default class Person{
-	constructor(name, age){
+export default class Person {
+	constructor(name, age) {
 		this.name = name;
 		this.age = age;
 	}
-	say(){
-		return `hello every one, my name is ${this.name}, and I'm ${this.age} years old. Oh, I am from ${Person.country}`;
+	say() {
+		return `halo every one, my name is ${this.name}, and I'm ${this.age} years old. Oh, I am from ${Person.country}`;
 	}
 }
